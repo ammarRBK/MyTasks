cc740164fd732d808228029b2fde90547f1dc465
ammarRBK <alshakmaskeain@gmail.com>
2025-05-29 22:54:40 +0300

    start building header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { Text, View, StyleSheet  } from 'react-native';
+import Icon from '@react-native-vector-icons/ionicons';
 
 const Header= () =>{
+    
     return (
         <View style={styles.headerContainer}>
-            <Text>Hello from header component</Text>
+            <Text style={styles.textHeader}>Hello from header component</Text>
+            <Text>
+                <Icon name="menu" size={20} color={"white"}/>
+            </Text>
         </View>
     )
 }
 
 const styles= StyleSheet.create({
     headerContainer:{
-        backgroundColor: "#fff"
+        backgroundColor: "lightblue",
+        flexDirection: "row",
+        height: 40,
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: 10
+    },
+    textHeader:{
+        color: "white",
+        marginLeft: 50
     }
 })
 
